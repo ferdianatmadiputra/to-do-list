2388e9fa0dfe4047677e59cd57ad9362b6ac52b8
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2021-01-27 20:23:26 +0700

    structure

---
+++ migrations/20210127125943-create-task.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      task_name: {
+        type: Sequelize.STRING
+      },
+      deadline: {
+        type: Sequelize.DATE
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Tasks');
+  }
+};
